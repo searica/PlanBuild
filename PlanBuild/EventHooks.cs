402750426ac276d70882023ad6546e1bae96ff78
searica <143636061+searica@users.noreply.github.com>
2024-04-16 08:25:06 -0700

    Add EventHooks patch class

---
+++ PlanBuild/EventHooks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HarmonyLib;
+
+namespace PlanBuild
+{
+
+    /// <summary>
+    ///     Event args class Player events
+    /// </summary>
+    internal class PlayerEventArgs : EventArgs
+    {
+        /// <summary>
+        ///     Reference to the player instance
+        /// </summary>
+        public Player player { get; set; }
+    }
+
+
+    [HarmonyPatch(typeof(EventHooks))]
+    internal static class EventHooks
+    {
+
+
+        /// <summary>
+        ///     Prefix
+        /// </summary>
+        public static event EventHandler<PlayerEventArgs> OnPlayerSettingUpPlacementGhost;
+
+        /// <summary>
+        ///     Prefix
+        /// </summary>
+        public static event EventHandler<PlayerEventArgs> OnPlayerUpdatingPlacementGhost;
+
+        public static event EventHandler<PlayerEventArgs> OnPlayerAddKnownPiece;
+
+        public static event EventHandler<PlayerEventArgs> OnPlayerHaveRequirements_Piece_RequirementMode;
+
+    }
+}
