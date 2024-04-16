95335667da9034d2bf7e0e2c23e0bb6cf973d136
searica <143636061+searica@users.noreply.github.com>
2024-04-16 09:56:46 -0700

    Add Ray Event args to get out Piece reference

--- PlanBuild/EventHooks.cs
+++ PlanBuild/EventHooks.cs
@@ -16,6 +16,41 @@ namespace PlanBuild
     }
 
 
+    /// <summary>
+    ///     Event args class PieceRayTest method
+    internal class RayTestEventArgs : EventArgs
+    {
+        /// <summary>
+        ///     Reference to the player instance
+        /// </summary>
+        public Player player { get; set; }
+
+        /// <summary>
+        ///     Refence to the result of the ray test.
+        /// </summary>
+        public Piece piece { get; set; }
+
+        /// <summary>
+        ///     Constructor for easier use when invoking events.
+        /// </summary>
+        /// <param name="player"></param>
+        public RayTestEventArgs(Player player, Piece piece) 
+        { 
+            this.player = player;
+            this.piece = piece;
+        }
+    }
+
+    /// <summary>
+    ///     Event args for placing a piece
+    /// </summary>
+    internal class PlacePieceEventArgs : EventArgs
+    {
+        /// <summary>
+        ///     Reference to the player instance
+        /// </summary>
+        public Player player { get; set; }
+
     [HarmonyPatch(typeof(EventHooks))]
     internal static class EventHooks
     {
